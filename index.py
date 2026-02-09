fd978b6056d1207696e2d1f50539867ced7f4d52
Youssef Elaichi <youssefvenom6002@proton.me>
2026-02-09 20:36:54 +0100

    We define the encrypt function that takes the text and the key as parameters

--- index.py
+++ index.py
@@ -2,6 +2,25 @@ def repeat_key(text, key):
     pepeated_key = ""
     key_index = 0
     for char in text:
-        if char.isalpha():
-            pepeated_key = key[key_index % len(key)]
+        if char.isalpha(): # We check if the character is an alphabet
+            pepeated_key = key[key_index % len(key)] # We repeat the key by using the modulus operator to wrap around the key
+            key_index += 1 # We increment the key index to move to the next character in the key
+        else:
+            pepeated_key = char # It is not an alphabet, so we keep it as it is 
+    return pepeated_key
+def encrypt(text, key): # We define the encrypt function that takes the text and the key as parameters
+    text = text.upper() # We change the text to uppercase to simplify the encryption process
+    key = repeat_key(text, key.upper()) # We repeat the key to match the length of the text
+    ciphertext = ""
+
+    for t_char, k_char in zip(text, key): # We iterate through the text and the repeated key simultaneously using the zip function
+        if t_char.isalpha(): # We check if the character is an alphabet
+            t_num = ord(t_char) - ord('A') # We convert the character to a number between 0 and 26 by subtracting the ASCII value of 'A' from the ASCII value of the character
+            k_num = ord(k_char) - ord('A') # We do the same for the key character
+            c_num = (t_num + k_num) % 26 # We add the text number and the key number and taket the modulus 26 to wrap around the alphabet
+            c_char = chr(c_num + ord('A')) # We convert the resulting number back to a character by adding the ASCII value of 'A'
+            ciphertext += c_char
+        else:
+            ciphertext += t_char
+    return ciphertext
 
